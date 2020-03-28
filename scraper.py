d7bfba070f00284b2605e0e2e530768605bf8127
Nozomi Okada <nokada@proofpoint.com>
2020-03-28 15:08:19 -0600

    Adding a constant for photos collection

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 from hashlib import md5
 
+from constants import PHOTOS_REF_NAME
 from provider import FirebaseAPIProvider, UnsplashAPIProvider
 
 firebase_provider = FirebaseAPIProvider()
@@ -7,4 +8,4 @@ photo_provider = UnsplashAPIProvider()
 
 for photo in photo_provider.get_photos(per_page=1, order_by='popular'):
     document_id = md5(photo.id.encode()).hexdigest()
-    firebase_provider.add_document(collection_id='photos', document_id=document_id, data=photo.dict())
+    firebase_provider.add_document(collection_id=PHOTOS_REF_NAME, document_id=document_id, data=photo.dict())
