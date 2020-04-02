438ad4d2836701b341742cc7d42fa685e005507f
Nozomi Okada <nokada@proofpoint.com>
2020-04-02 10:42:02 -0600

    Adding read_scout_photos_from_json

--- scraper.py
+++ scraper.py
@@ -4,6 +4,7 @@ import time
 from datetime import datetime
 from hashlib import md5
 
+from dateutil.parser import parse
 from google.cloud.firestore_v1 import GeoPoint
 from unsplash import UnsplashError
 
@@ -33,7 +34,7 @@ class DataService:
             collection_id=PHOTOS_REF_NAME, document_id=document_id, data=photo.dict()
         )
 
-    def write_scout_photos_to_file(self):
+    def write_scout_photos_to_json(self):
         data = {}
         for doc in self.firebase_provider.get_documents(PHOTOS_REF_NAME):
             doc_id, doc_data = doc[0], doc[1]
@@ -41,6 +42,22 @@ class DataService:
             data[doc_id] = doc_data
             with open('photos.json', 'w') as file:
                 json.dump(data, file, indent=4, sort_keys=True, default=self.support_scout_data_types)
+
+    def read_scout_photos_from_json(self):
+        with open('photos.json', 'r') as file:
+            data = json.load(file, object_hook=self.parse_scout_data_types)
+
+        for key, value in data.items():
+            data_service.firebase_provider.add_document(
+                collection_id="photos_test", document_id=key, data=value
+            )
+
+    @staticmethod
+    def parse_scout_data_types(json_dict):
+        for key, value in json_dict.items():
+            if key == 'created':
+                json_dict[key] = parse(value)
+        return json_dict
 
     @staticmethod
     def support_scout_data_types(o):
@@ -71,5 +88,9 @@ class DataService:
 
 
 data_service = DataService()
-data_service.write_scout_photos_to_file()
+
 # data_service.execute_scraping_cycle(page_number=126)
+
+# data_service.write_scout_photos_to_json()
+
+data_service.read_scout_photos_from_json()
